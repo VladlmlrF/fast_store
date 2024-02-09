a399654843cb45f13398a3bb9b93a57fe8260b8c
VladlmlrF <vladimirfalin84@gmail.com>
2024-02-09 22:31:18 +0700

    create model User

---
+++ fast_store/core/models/user.py
@@ -0,0 +1,33 @@
+from enum import Enum
+from typing import TYPE_CHECKING
+
+from sqlmodel import Field
+from sqlmodel import Relationship
+
+from .base import Base
+
+if TYPE_CHECKING:
+    from .order import Order
+    from .cart import Cart
+    from .profile import Profile
+
+
+class Role(str, Enum):
+    USER = "USER"
+    ADMIN = "ADMIN"
+    SUPER_ADMIN = "SUPER_ADMIN"
+
+
+class User(Base, table=True):
+    username: str = Field(index=True)
+    email: str
+    hashed_password: str
+    is_active: bool = Field(default=True)
+    role: Role = Field(default=Role.USER)
+    orders: list["Order"] = Relationship(back_populates="user")
+    cart: "Cart" = Relationship(
+        back_populates="user", sa_relationship_kwargs={"uselist": False}
+    )
+    profile: "Profile" = Relationship(
+        back_populates="user", sa_relationship_kwargs={"uselist": False}
+    )
