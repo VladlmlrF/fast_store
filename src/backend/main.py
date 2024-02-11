4a55d152abb68fc009bc00e267a373322dc7c094
VladlmlrF <vladimirfalin84@gmail.com>
2024-02-11 17:10:08 +0700

    add auth_router to main.py

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,8 +1,18 @@
+import uvicorn
 from fastapi import FastAPI
 
-app = FastAPI()
+from src.backend.app.auth.views import router as auth_router
+
+app = FastAPI(title="Fast Store")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello"}
+
+
+app.include_router(auth_router, prefix="/auth")
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
