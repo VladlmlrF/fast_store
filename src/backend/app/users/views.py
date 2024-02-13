5fcaeb1e9506c0f234015f4512e85fdbc02e2724
VladlmlrF <vladimirfalin84@gmail.com>
2024-02-13 22:00:41 +0700

    update get_user

--- src/backend/app/users/views.py
+++ src/backend/app/users/views.py
@@ -91,11 +91,6 @@ async def get_user(
 ):
     await admin_required(session=session, current_user_name=current_user_name)
     user = await crud.get_user_by_username(session=session, username=username)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=f"User {username} not found!",
-        )
     return user
 
 
