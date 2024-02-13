b1cb65cd1fb2a7a1340fe8882884859f87ecb8ae
VladlmlrF <vladimirfalin84@gmail.com>
2024-02-13 22:19:27 +0700

    add category_router

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 
 from src.backend.app.auth.views import router as auth_router
+from src.backend.app.categories.views import router as category_router
 from src.backend.app.users.views import router as user_router
 
 app = FastAPI(title="Fast Store")
@@ -14,6 +15,7 @@ async def root():
 
 app.include_router(auth_router, prefix="/auth")
 app.include_router(user_router, prefix="/user")
+app.include_router(category_router, prefix="/category")
 
 
 if __name__ == "__main__":
