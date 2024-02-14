09b5e31f810290d02aca7ce40b4cef743073bebb
VladlmlrF <vladimirfalin84@gmail.com>
2024-02-14 18:26:44 +0700

    add product_router

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 
 from src.backend.app.auth.views import router as auth_router
 from src.backend.app.categories.views import router as category_router
+from src.backend.app.products.views import router as product_router
 from src.backend.app.users.views import router as user_router
 
 app = FastAPI(title="Fast Store")
@@ -16,6 +17,7 @@ async def root():
 app.include_router(auth_router, prefix="/auth")
 app.include_router(user_router, prefix="/user")
 app.include_router(category_router, prefix="/category")
+app.include_router(product_router, prefix="/product")
 
 
 if __name__ == "__main__":
